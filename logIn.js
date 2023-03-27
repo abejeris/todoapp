44bf14158e37563250787e1c9f2353278ca59977
Arvydas Pečiulis <arvydas.peciulis23@gmail.com>
2023-03-27 20:24:44 +0300

    todofirstJS

--- logIn.js
+++ logIn.js
@@ -3,21 +3,27 @@ const errorMessage = document.querySelector("#errorMessage");
 errorMessage.style.color = "red";
 
 signUpForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const username = document.querySelector("#name").value;
-  const password = document.querySelector("#password").value;
+	e.preventDefault();
+	const username = document.querySelector("#name").value;
+	const password = document.querySelector("#password").value;
 
-  if (password === "" || username === "") {
-    errorMessage.textContent = "Please enter all values";
-  } else if (localStorage.getItem(username)) {
-    const loginDetails = JSON.parse(localStorage.getItem(username));
-    if (
-      username === loginDetails.username &&
-      password === loginDetails.password
-    ) {
-      alert("Logged in");
-    } else {
-      errorMessage.textContent = "Wrong credentials";
-    }
-  }
+	if (password === "" || username === "") {
+		errorMessage.textContent = "Please enter all values";
+	} else if (localStorage.getItem(username)) {
+		const loginDetails = JSON.parse(localStorage.getItem(username));
+		if (
+			username === loginDetails.username &&
+			password === loginDetails.password
+		) {
+			// When user logs in create logged in user ogj and store it in local storage.
+			const currentAccount = {
+				username: username,
+			};
+			localStorage.setItem("LoggedInAs", JSON.stringify(currentAccount));
+			//redirect to page ""
+			window.location.href = "todo.html";
+		} else {
+			errorMessage.textContent = "Wrong credentials";
+		}
+	}
 });
