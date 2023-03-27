39f09aba2d84d04b0cb3ad106d7ac53d0a5a5461
Arvydas Pečiulis <arvydas.peciulis23@gmail.com>
2023-03-27 21:23:34 +0300

    works with logins on reefresh todos stay also edit btn works

--- todo.js
+++ todo.js
@@ -1,51 +1,68 @@
 const welcomeUser = document.querySelector("#usernameLogo");
-//get stored obj and take username
 const getUser = JSON.parse(localStorage.getItem("LoggedInAs"));
 welcomeUser.textContent = getUser.username;
 
 const todoInput = document.querySelector("#todo");
 const addBtn = document.querySelector("#add");
 
-// function for button add
+const userTodosKey = `todos-${getUser.username}`; // Use username as part of key
+
+let todos = JSON.parse(localStorage.getItem(userTodosKey)) || [];
+
+function renderTodos() {
+	const todoListContainer = document.querySelector("#todoList");
+	todoListContainer.innerHTML = ""; // Clear list before rendering
+
+	todos.forEach((todo, index) => {
+		const todoListDiv = document.createElement("div");
+		todoListDiv.classList.add("todoDiv");
+		todoListContainer.append(todoListDiv);
+
+		const todoText = document.createElement("span");
+		todoText.classList.add("todoText");
+		todoText.textContent = todo;
+		todoListDiv.append(todoText);
+
+		const deleteBtn = document.createElement("button");
+		deleteBtn.classList.add("deleteBtn");
+		deleteBtn.textContent = "delete";
+		todoListDiv.append(deleteBtn);
+
+		deleteBtn.addEventListener("click", function () {
+			todos.splice(index, 1);
+			localStorage.setItem(userTodosKey, JSON.stringify(todos));
+			renderTodos();
+		});
+		const editBtn = document.createElement("button");
+		editBtn.classList.add("editBtn");
+		editBtn.textContent = "edit";
+		todoListDiv.append(editBtn);
+
+		editBtn.addEventListener("click", function () {
+			const newTodoText = prompt("Edit your to-do item:", todoText.textContent);
+			if (newTodoText !== null && newTodoText !== "") {
+				todoText.textContent = newTodoText;
+			}
+		});
+
+		const favBtn = document.createElement("button");
+		favBtn.classList.add("favBtn");
+		favBtn.textContent = "favorite";
+		todoListDiv.append(favBtn);
+	});
+}
+
+renderTodos();
+
 addBtn.addEventListener("click", function (e) {
 	e.preventDefault();
 
-	const todoListContainer = document.querySelector("#todoList");
-	const todoListDiv = document.createElement("div");
-	todoListDiv.classList.add("todoDiv");
-	todoListContainer.append(todoListDiv);
+	const newTodo = todoInput.value.trim();
 
-	// adding todo text
-	const todoText = document.createElement("span");
-	todoText.classList.add("todoText");
-	todoText.textContent = todoInput.value;
-	todoListDiv.appendChild(todoText);
-
-	// adding delete button
-	const deleteBtn = document.createElement("button");
-	deleteBtn.classList.add("deleteBtn");
-	deleteBtn.textContent = "delete";
-	todoListDiv.appendChild(deleteBtn);
-
-	// adding event listener to delete button
-	deleteBtn.addEventListener("click", function () {
-		todoListDiv.remove();
-	});
-	const editBtn = document.createElement("button");
-	editBtn.classList.add("editBtn");
-	editBtn.textContent = "edit";
-	todoListDiv.appendChild(editBtn);
-
-	editBtn.addEventListener("click", function () {
-		// edit function
-	});
-
-	const favBtn = document.createElement("button");
-	favBtn.classList("favBtn");
-	favBtn.textContent = "favorite";
-	todoListDiv.appendChild(favBtn);
-
-	editBtn.addEventListener("click", function () {
-		// favorite function
-	});
+	if (newTodo) {
+		todos.push(newTodo);
+		localStorage.setItem(userTodosKey, JSON.stringify(todos));
+		renderTodos();
+		todoInput.value = "";
+	}
 });
